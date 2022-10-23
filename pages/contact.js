9bf3eae04537957e755247babcd7a74d65010665
Ali <iamaliahad@outlook.com>
2022-10-24 05:51:35 +0800

    add netlify contact form

---
+++ pages/contact.js
@@ -0,0 +1,89 @@
+import siteMetadata from '@/data/siteMetadata'
+import projectsData from '@/data/projectsData'
+
+import { PageSEO } from '@/components/SEO'
+
+export default function Projects() {
+  return (
+    <>
+      <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Contact
+          </h1>
+          {/* <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Showcase your projects with a hero image (16 x 9)
+          </p> */}
+        </div>
+        <div className="container py-12">
+          <section className="bg-white dark:bg-gray-900">
+            <div className="mx-auto max-w-screen-md py-8 px-4 lg:py-16">
+              <h2 className="mb-4 text-center text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
+                Contact Me
+              </h2>
+              <p className="mb-8 text-center font-light text-gray-500 dark:text-gray-400 sm:text-xl lg:mb-16">
+                {' '}
+                Feel free to contact me. Let's discussion to about your project, Startup and Idea.
+                😊{' '}
+              </p>
+              <form action="#" className="space-y-8" data-netlify="true" name="contact">
+                <input type="hidden" name="form-name" value="contact" />
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+                    placeholder="yourName@example.com"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="subject"
+                    className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    id="subject"
+                    className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+                    placeholder="Let me know how I can help you"
+                    required
+                  />
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="message"
+                    className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-400"
+                  >
+                    Your message
+                  </label>
+                  <textarea
+                    id="message"
+                    rows="6"
+                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+                    placeholder="Leave a comment..."
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className="rounded-lg bg-primary-700 py-3 px-5 text-center text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-fit"
+                >
+                  Send message
+                </button>
+              </form>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  )
+}
