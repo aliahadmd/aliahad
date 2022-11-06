7875bc3504499af10e4bdd8f8e329284cc246112
Ali <90032081+iamaliahad@users.noreply.github.com>
2022-11-07 04:03:25 +0800

    project and avatar update

--- data/projectsData.js
+++ data/projectsData.js
@@ -8,20 +8,28 @@ const projectsData = [
     gref: 'https://github.com/iamAliAhad/full-stack-ecommerce',
   },
   {
-    title: 'Phone Ads Landing page',
-    technology: '#CSS #React #threejs #GSAP',
-    description: `A landing page for Phone launch. `,
-    imgSrc: '/static/images/p_phone.png',
-    href: 'https://landing-page-for-phone-sample-design-reactjs-gsap-three-js.vercel.app/',
-    gref: 'https://github.com/iamaliahad/Landing-page-for-phone-sample-design-reactjs-GSAP-threeJs',
-  },
-  {
     title: 'Task Manager System',
     technology: '#ExpressJs #MongoDB #ReactJs #NodeJs #TailwindCSS',
     description: `Simple Task Manager System. Built with expressJs, NodeJs, React, MongoDB and TailwindCSS `,
     imgSrc: '/static/images/task.png',
     href: 'https://github.com/iamAliAhad/Task-manager-System/blob/master/README.md',
     gref: 'https://github.com/iamAliAhad/Task-manager-System',
+  },
+  {
+    title: 'Full Stack Chat web Application',
+    technology: '#ExpressJs #MongoDB #ReactJs #NodeJs #StyledComponents',
+    description: `Fully functional chat web application. Built with expressJs, NodeJs, React, MongoDB and StyledComponents `,
+    imgSrc: '/static/images/chatapp.png',
+    href: 'https://github.com/iamAliAhad/ChatApp/blob/master/README.md',
+    gref: 'https://github.com/iamAliAhad/ChatApp',
+  },
+  {
+    title: 'Phone Ads Landing page',
+    technology: '#CSS #React #threejs #GSAP',
+    description: `A landing page for Phone launch. `,
+    imgSrc: '/static/images/p_phone.png',
+    href: 'https://landing-page-for-phone-sample-design-reactjs-gsap-three-js.vercel.app/',
+    gref: 'https://github.com/iamaliahad/Landing-page-for-phone-sample-design-reactjs-GSAP-threeJs',
   },
 ]
 
