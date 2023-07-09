e2100bab8b3eb5fa64ee282db947782ec12856ba
ali <iamaliahad@outlook.com>
2023-07-09 19:24:21 +0800

    homepage changes

--- data/authors/default.md
+++ data/authors/default.md
@@ -1,20 +1,15 @@
 ---
 name: Ali Ahad
-avatar: /static/images/avatar.jpg
+avatar: /static/images/avatar.png
 occupation: Software Engineer
 company: Freelancer
 skill: 'HTML, CSS, JavaScript, TypeScript, ReactJs, NodeJs, MongoDB, SQL, Python, C++, Figma'
-email: hi@aliahad.com
-twitter: https://twitter.com/iamAliAhad
-linkedin: https://www.linkedin.com/iamaliahad
-github: https://github.com/iamAliAhad
+email: ali@aliahad.com
+twitter: https://twitter.com/thealiahad
+linkedin: https://www.linkedin.com/in/thealiahad
+github: https://github.com/thealiahad
 lab: https://lab.aliahad.com
 ---
 
-I am **Ali Ahad**, Studying Computer Science with experience working across the **full-stack software development**.
-
-As an Artificial Intelligence Engineer and learner, I am driven by my passion for using technology to solve **complex problems** and improve the lives of people. I am constantly seeking out new ways to push the boundaries of what is possible with AI, and I am always looking for ways to apply my skills and knowledge to **real-world** challenges.
-
-Overall, I am excited about the potential for AI to make a positive impact in the world, and I am committed to using my skills and knowledge to help drive this change. Whether I am working on a project for a client, collaborating with a team, or learning something new, I am always motivated by the opportunity to make a difference and create meaningful solutions.
-
+I am **Ali Ahad**, Studying Masters of Computer Science with experience working across the **full-stack software development**.
 Freelance Full-stack Developer and SEO expert! Hard-working web developer with a flair for creating elegant solutions in the least amount of time. 🚀
--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -11,7 +11,8 @@ export default function AuthorLayout({ children, frontMatter }) {
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
       <div className="divide-y">
-        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+        {/* <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0"> */}
+        <div className="items-start space-y-2">
           <div className="flex flex-col items-center pt-8">
             <Image
               src={avatar}
@@ -23,12 +24,12 @@ export default function AuthorLayout({ children, frontMatter }) {
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            {/* <div className="flex space-x-3 pt-6">
+            <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="twitter" href={twitter} />
-            </div> */}
+            </div>
             {/* <Link href={lab}>
               <button className="group relative mb-2 mr-2 mt-3 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-purple-500 to-pink-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-purple-200 group-hover:from-purple-500 group-hover:to-pink-500 dark:text-white dark:focus:ring-purple-800">
                 <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
